26f7e3983322f206c3cd3f08bd8419850e52eb09
Raphael <rparodi@student.42.fr>
2024-10-25 12:56:51 +0200

    Adding the start of ex02

---
+++ cpp00/ex02/Account.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rparodi <rparodi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 16:11:46 by rparodi           #+#    #+#             */
+/*   Updated: 2024/10/23 16:52:20 by rparodi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+Account::Account()
+{
+	_nbAccounts = 0;
+	_totalAmount = 0;
+	_totalNbDeposits = 0;
+	_totalNbWithdrawals = 0;
+}
+
+static int	Account::getNbAccounts()
+{
+	t.size();
+}
+
+static int	Account::getTotalAmount();
+
+static int	Account::getNbDeposits();
+
+static int	Account::getNbWithdrawals();
+
+static void	Account::displayAccountsInfos();
