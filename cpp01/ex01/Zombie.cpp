0271d4b2dee3a8424b51da28f36241659e71db4b
Raphael <rparodi@student.42.fr>
2024-11-05 12:48:37 +0100

    style: added color to the ex01 of the module 01

--- cpp01/ex01/Zombie.cpp
+++ cpp01/ex01/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: rparodi <rparodi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 16:10:21 by rparodi           #+#    #+#             */
-/*   Updated: 2024/11/04 20:22:57 by rparodi          ###   ########.fr       */
+/*   Updated: 2024/11/05 12:39:33 by rparodi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ Zombie::Zombie()
 
 Zombie::~Zombie()
 {
-	std::cout << "Zombie's hunter: killed " << _name << std::endl;
+	std::cout << CLR_YELLOW << "Zombie's hunter: killed " << CLR_RESET << CLR_GOLD << _name << CLR_RESET << std::endl;
 }
 
 void Zombie::setName( std::string name )
@@ -29,5 +29,5 @@ void Zombie::setName( std::string name )
 
 void Zombie::announce()
 {
-	std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << CLR_BOLD_GREEN << _name << CLR_RESET << CLR_LIGHT_GREEN <<": BraiiiiiiinnnzzzZ..." << CLR_RESET << std::endl;
 }
--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rparodi <rparodi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 15:48:46 by rparodi           #+#    #+#             */
-/*   Updated: 2024/11/04 20:38:40 by rparodi          ###   ########.fr       */
+/*   Updated: 2024/11/05 12:42:33 by rparodi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,20 @@ int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
-		std::cerr << "Error:\nYou have to use a numeric argument !\nRemember the syntax:\n\t" << argv[0] << " <name> <size>" << std::endl;
+		std::cerr << CLR_RED << "Error:\nYou have to use a numeric argument !\n" << CLR_RESET << CLR_BLUE << "Remember the syntax:\n\t" << argv[0] << " <name> <size>" << CLR_RESET << std::endl;
 		return 1;
 	}
 	std::string str(argv[2]);
 	if (str.length() > 10)
 	{
-		std::cerr << "Error:\nYou can only have an integer in parameters" << std::endl;
+		std::cerr << CLR_RED << "Error:\nYou can only have an integer in parameters" << CLR_RESET << std::endl;
 		return 2;
 	}
 	int	size = std::atoll(argv[2]);
 	std::string name = std::string(argv[1]);
 	if (size <= 0)
 	{
-		std::cerr << "Error:\nThe number have to be positive" << std::endl;
+		std::cerr << CLR_RED << "Error:\nThe number have to be positive" << CLR_RESET << std::endl;
 		return 3;
 	}
 	
