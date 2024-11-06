4e8dc67e653f6a636bcb89d070b8b8c55fd745cc
Raphael <rparodi@student.42.fr>
2024-11-06 16:41:58 +0100

    update: finishing for good the last exercice

--- cpp01/ex04/err_msg.hpp
+++ cpp01/ex04/err_msg.hpp
@@ -6,7 +6,7 @@
 /*   By: rparodi <rparodi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 15:26:18 by rparodi           #+#    #+#             */
-/*   Updated: 2024/11/05 15:53:05 by rparodi          ###   ########.fr       */
+/*   Updated: 2024/11/06 16:35:49 by rparodi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,6 @@
 #define ERR_PREFIX "Error:\n"
 #define ARGS_MISSING "Please give 3 arguments: <filename> <to_replace> <by_word> !"
 #define FILE_CANT_OPEN ": The file cannot be open !"
+#define FILE_CANT_READ ": The file connot be read !"
+
 #endif
--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,12 +6,14 @@
 /*   By: rparodi <rparodi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 15:24:29 by rparodi           #+#    #+#             */
-/*   Updated: 2024/11/06 16:09:06 by rparodi          ###   ########.fr       */
+/*   Updated: 2024/11/06 16:36:48 by rparodi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "err_msg.hpp"
 #include "sed.hpp"
 #include <fstream>
+#include <iostream>
 
 int	main(int argc, char *argv[])
 {
@@ -23,15 +25,15 @@ int	main(int argc, char *argv[])
 	std::ifstream input(argv[1]);
 	if (!input)
 	{
-		std::cout << CLR_RED ERR_PREFIX << argv[1] << FILE_CANT_OPEN << CLR_RESET << std::endl;
+		std::cerr << CLR_RED ERR_PREFIX << argv[1] << FILE_CANT_OPEN CLR_RESET << std::endl;
 		return (1);
 	}
 	Sed new_sed = Sed(argv, &input);
 	std::ofstream output(new_sed.getFilename());
-    if (!output.is_open()) {
-        std::cerr << "Error: Could not open the file for writing." << std::endl;
-        return 1;
-    }
+	if (!output.is_open()) {
+		std::cerr << CLR_RED ERR_PREFIX << new_sed.getFilename() << FILE_CANT_OPEN CLR_RESET << std::endl;
+		return 1;
+	}
 	while (new_sed.line(output))
 	{
 	}
