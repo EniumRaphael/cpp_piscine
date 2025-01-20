0188957b104c414efc7110001f00059f9ec30dc8
Raphael <rparodi@student.42.fr>
2025-01-20 23:34:40 +0100

    adding the start of the new class with heritage

---
+++ cpp03/ex01/ScavTrap.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rparodi <rparodi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/20 23:13:16 by rparodi           #+#    #+#             */
+/*   Updated: 2025/01/20 23:17:49 by rparodi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include <iostream>
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap {
+	public:
+
+	private:
+};
+
+#endif
