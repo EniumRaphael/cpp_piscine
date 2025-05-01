5ddecf46b7004aec278d03f2001d533c0511f3e9
Raphael <rparodi@student.42.fr>
2025-05-01 16:34:45 +0200

    feat(09): adding the start w/ debug mode

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,8 +6,27 @@
 /*   By: rparodi <rparodi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 14:45:25 by rparodi           #+#    #+#             */
-/*   Updated: 2025/05/01 14:45:29 by rparodi          ###   ########.fr       */
+/*   Updated: 2025/05/01 16:06:02 by rparodi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+#include <iostream>
+
+#define CLR_RESET "\033[0m"
+
+#define CLR_BLACK "\033[0;30m"
+#define CLR_RED "\033[0;31m"
+#define CLR_GREEN "\033[0;32m"
+#define CLR_YELLOW "\033[0;33m"
+#define CLR_BLUE "\033[0;34m"
+#define CLR_MAGENTA "\033[0;35m"
+#define CLR_CYAN "\033[0;36m"
+#define CLR_WHITE "\033[0;37m"
+#define CLR_GOLD "\033[38;5;220m"
+#define CLR_GREY "\033[38;5;240m"
+
+#ifndef DEBUG
+#define DEBUG 0
+#endif
--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,8 +6,17 @@
 /*   By: rparodi <rparodi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 14:45:40 by rparodi           #+#    #+#             */
-/*   Updated: 2025/05/01 14:45:41 by rparodi          ###   ########.fr       */
+/*   Updated: 2025/05/01 16:28:50 by rparodi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PmergeMe.hpp"
+#include <iostream>
 
+int	main(int argc, char **argv) {
+	if (DEBUG)
+		std::cerr << CLR_MAGENTA << "Warning: DEBUG_MODE is now enable" << CLR_RESET << std::endl << std::endl;
+	if (argc < 2)
+		std::cerr << CLR_RED << "Usage:\t" << argv[0] << " <number> <number> ..." << CLR_RESET << std::endl;
+	return (0);
+}
